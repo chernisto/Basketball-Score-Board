e66592d95765e6e21f2eca7d22704b18345954a4
chernisto <87658631+chernisto@users.noreply.github.com>
2022-11-04 11:51:11 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+//HOME
+
+let scoreHome = 0;
+
+function zeroHome(){
+    scoreHome = 0;
+    document.getElementById("score-home").textContent = scoreHome;
+}
+
+function addOneHome(){
+    scoreHome += 1;
+    document.getElementById("score-home").textContent = scoreHome;
+}
+
+function addTwoHome(){
+    scoreHome += 2;
+    document.getElementById("score-home").textContent = scoreHome;
+}
+
+function addThreeHome(){
+    scoreHome += 3;
+    document.getElementById("score-home").textContent = scoreHome;
+}
+
+
+//AWAY
+
+let scoreAway = 0;
+
+function zeroAway(){
+    scoreAway = 0;
+    document.getElementById("score-away").textContent = scoreAway;
+}
+
+function addOneAway(){
+    scoreAway += 1;
+    document.getElementById("score-away").textContent = scoreAway;
+}
+
+function addTwoAway(){
+    scoreAway += 2;
+    document.getElementById("score-away").textContent = scoreAway;
+}
+
+function addThreeAway(){
+    scoreAway += 3;
+    document.getElementById("score-away").textContent = scoreAway;
+}
+
+
+
+
